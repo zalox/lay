d43bd7e6238273c1a728cf67f63d30bdec0455bc
Jørgen Lien Sellæg <j.sellaeg@sportradar.com>
2018-11-06 19:42:01 +0100

    setup webpack

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Lay = () => <div>Lay</div>;
+
+render(
+  <Lay />,
+  /* eslint-disable-next-line no-undef */
+  document.getElementById('lay'),
+);
